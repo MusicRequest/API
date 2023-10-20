5f3b65dc88840aa8acdae57938992c7ceb14661d
FlorianBEME <florianbme@gmail.com>
2023-10-20 23:51:34 +0200

    Correction logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,4 +4,4 @@ const LOG_LEVEL = process.env.NODE_ENV === "production" ? "info" : "log";
 
 const logger = tracer.colorConsole({ level: LOG_LEVEL });
 
-module.exports = logger;
+export default logger;
