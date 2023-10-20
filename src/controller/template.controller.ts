83eb4efcb978a57b220e54dacb6af86f12c18e81
FlorianBEME <florianbme@gmail.com>
2023-10-20 23:46:05 +0200

    Correction template

--- src/controller/template.controller.ts
+++ src/controller/template.controller.ts
@@ -1,23 +1,90 @@
 import { NextFunction, Request, Response } from "express";
+import { db } from "../utils/db.server";
+import logger from "../utils/logger";
 
-const getAll = (req: Request, res: Response, next: NextFunction) => {
-  console.log("Get ALL");
+const ENTITY = "";
+
+const getAll = async (req: Request, res: Response): Promise<Response> => {
+  logger.info(`Récupération de collection (${ENTITY})`);
+  try {
+    const eventList = await db[ENTITY].findMany({});
+    return res.json(eventList).status(200);
+  } catch (e) {
+    logger.error("Une erreur c'est produite");
+    throw new Error("Error...");
+  }
 };
 
-const getById = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`Get ${req.params.id}`);
+const getById = async (req: Request, res: Response): Promise<Response> => {
+  if (!req.params.id) return res.sendStatus(400);
+  logger.info(`Récupération id:${req.params.id}`);
+
+  try {
+    const event = await db[ENTITY].findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!event) res.status(404);
+
+    return res.json(event).status(200);
+  } catch (e) {
+    logger.error("Une erreur c'est produite");
+    throw new Error("Error...");
+  }
 };
 
-const post = (req: Request, res: Response, next: NextFunction) => {
-  console.log("post");
+const post = async (req: Request, res: Response) => {
+  logger.info(`Création de ressouce id:${req.params.id}`);
+
+  try {
+    const event = await db[ENTITY].create({
+      ...req.body,
+    });
+
+    return res.json(event).status(201);
+  } catch (e) {
+    logger.error("Une erreur c'est produite");
+    throw new Error("Error...");
+  }
 };
 
-const put = (req: Request, res: Response, next: NextFunction) => {
-  console.log("put");
+const put = async (req: Request, res: Response) => {
+  logger.info(`Modification de ressouce id:${req.params.id}`);
+
+  try {
+    const event = await db[ENTITY].update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        ...req.body,
+      },
+    });
+
+    return res.json(event).status(202);
+  } catch (e) {
+    logger.error("Une erreur c'est produite");
+    throw new Error("Error...");
+  }
 };
 
-const remove = (req: Request, res: Response, next: NextFunction) => {
-  console.log("remove");
+const remove = async (req: Request, res: Response) => {
+  logger.info(`Suppression de ressouce id:${req.params.id}`);
+
+  try {
+    await db[ENTITY].delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.status(204);
+  } catch (e) {
+    logger.error("Une erreur c'est produite");
+    throw new Error("Error...");
+  }
 };
 
 module.exports = {
