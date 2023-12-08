14b035494731a4ce8ef0350f425616ce42e2e84e
FlorianBEME <florianbme@gmail.com>
2023-12-09 00:13:23 +0100

    Sécurité

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import verifyJWT from "../middleware/verifyJWT";
 
 const router: Router = require("express").Router();
 const eventController = require("../controller/event.controller");
 
 router.get("/", eventController.getAll);
 router.get("/:id", eventController.getById);
-router.post("/", eventController.post);
-router.put("/:id", eventController.put);
-router.delete("/:id", eventController.remove);
+router.post("/", verifyJWT, eventController.post);
+router.put("/:id", verifyJWT, eventController.put);
+router.delete("/:id", verifyJWT, eventController.remove);
 
 module.exports = router;
