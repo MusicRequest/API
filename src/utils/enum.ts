69ec257da17003c6f6af1f75a04cb9df0f43142e
FlorianBEME <florianbme@gmail.com>
2024-06-15 00:11:39 +0200

    Ajout type et enum

---
+++ src/utils/enum.ts
@@ -0,0 +1,10 @@
+export enum Entity {
+  Event = "event",
+  Visitor = "visitor",
+  ActivityLogs = "activityLogs",
+}
+
+export enum ActivityLogsType {
+  User = "user",
+  Music = "music",
+}
---
+++ src/utils/type.ts
@@ -0,0 +1,9 @@
+export type Event = {
+  id: string;
+  name: string;
+  background_image?: string;
+  options: any;
+  participants: any[];
+  createdAt: string;
+  updatedAt: string;
+};
